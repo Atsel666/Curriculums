cf8c102ad9031f6f30719cb37ddc80dd674c4169
Maritrini Velázquez <a329675@uach.mx>
2021-05-27 14:24:10 -0600

    clase Curriculum Vitae

---
+++ app/src/main/java/com/example/myapp/models/CurriculumVitae.java
@@ -0,0 +1,10 @@
+package com.example.myapp.models;
+
+public class CurriculumVitae {
+    public String name;
+    public String bio;
+    public String prepa;
+    public String uni;
+    public String ingles;
+    public  String Skills;
+}
